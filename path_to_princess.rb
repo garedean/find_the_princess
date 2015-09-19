00610ca977d553a5a6c9deaa5924caa6ec21197e
garedean <garrettdolson@gmail.com>
2015-09-19 11:49:05 -0700

    Add logic outputting most efficient path to the Princess

--- path_to_princess.rb
+++ path_to_princess.rb
@@ -5,9 +5,27 @@ require './lib/player'
 
 moves = []
 
-board        = Board.new
+board                 = Board.new
+board_helper          = BoardHelper.new(board)
 
-# used for finding Mario and Princess coodinates, distances between them, best moves
-board_helper = BoardHelper.new(board)
-mario        = Mario.new(board, board_helper, "M")
-princess     = Player.new(board, board_helper, "P")
+mario                 = Mario.new(board, board_helper, "M")
+board_helper.mario    = mario
+mario.location        = board_helper.get_player_coodinates(mario)
+
+princess              = Player.new(board, board_helper, "P")
+board_helper.princess = princess
+princess.location     = board_helper.get_player_coodinates(princess)
+
+until mario.found_princess?(princess)
+  moves << mario.move_toward_princess
+end
+
+puts "\nWould you like to see the quickest way to get to Princess? Hit 'Enter' to see the moves"
+
+if gets == "\n"
+  puts "The quickest way to get to the princess is with #{moves.size} moves:"
+  moves.each do |move|
+    puts move
+    sleep(0.2)
+  end
+end
